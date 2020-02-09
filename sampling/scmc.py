7b0d3eebeadd1395674c0d84f435f3bc15e1511c
odibua <odibua@gmail.com>
2020-02-08 20:12:25 -0800

    Complete weight modification and resample from candidates based on modified weights

--- sampling/scmc.py
+++ sampling/scmc.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import choices
 from scipy.stats import norm
 from smt.sampling_methods import LHS
 from typing import List, Callable
@@ -29,6 +30,7 @@ class constrained_scmc:
         self.norm_cdf = norm.cdf
         self.scale = 1.0
 
+
     def modify_weights(self, tau_t_1: float, tau_t: float):
         """
         Modify the weights at time step t based on constraints and current candidate
@@ -41,8 +43,18 @@ class constrained_scmc:
         self.calc_wn(tau_t_1, tau_t)
 
         #M Modify weights
+        norm_constant = 0
         for idx, W in enumerate(self.W):
             self.W[idx] = W*self.w[idx]
+            norm_constant = norm_constant + self.W[idx]
+        self.W = self.W/norm_constant
+
+    def resample_candidates(self):
+        """
+        Resample from candidate points based on updated weights
+        :return:
+        """
+        self.x = np.array(choices(population=self.x, weights=self.W, k=self.N))
 
 
     def calc_wn(self, tau_t_1: float, tau_t: float):
