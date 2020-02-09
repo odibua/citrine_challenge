38d7ac7c9486e0c673affaaeae70ed0cdce812e4
odibua <odibua@gmail.com>
2020-02-09 13:14:48 -0800

    Write metropolis hastings class that will be used in SCMC. Currently written in simplest form. Will iterate if needed

--- sampling/metropolis_hastings.py
+++ sampling/metropolis_hastings.py
@@ -1,18 +1,60 @@
 import numpy as np
-from scipy.stats import norm
+from scipy.stats import norm, uniform
 from typing import Callable
 
 norm_rvs = norm.rvs
+norm_pdf = norm.pdf
+
 class metropolis_hastings:
-    def __init__(self, x: np.Array[float], trans_kern: Callable = norm_rvs,  T: int = 5):
+    def __init__(self, x: np.array[np.Array[float]], pi: Callable,  T: int = 5, proposal_distrib: Callable = norm_pdf, trans_kern: Callable = norm_rvs, loc: float = 0.0, scale: float = 1.0):
         """
         Initialize basic parameters used for metropolis hastings
         :param x: Initial vector from which new samples will be generated
-        :param trans_kern: Transition kernel used to generate new samples. Defaults to N(0,1)
+        :param pi: Function that represents target distribution
         :param T: Number of steps taking in metropolis hastings
+        :param trans_kern: Transition kernel used to generate new samples. Defaults to sampling N(0,1)
+        :param loc: Mean of transition kernel
+        :param scale: Standard deviation of transition kernel
         """
-        self. x = x
+
+        self. x = [x] if len(x.shape)==2 else x
+        self.pi = pi
         self.trans_kern = trans_kern
         self.T = T
+        self.loc = loc
+        self.scale = scale
+
+        def proposal(xi, xi_1):
+            return proposal_distrib(xi-xi_1, loc=self.loc, scale=self.scale)
+        self.proposal_distrib = proposal
+
+    def gibbs_type(self, proposal_distrib: Callable = None, trans_kern: Callable = None):
+        """
+        Perform metropolis hastings using one element of each candidate at a time.
+        :param proposal_distrib: Proposal distribution that evaluates pdf of proposed point conditional on new one
+        :param trans_kern:  Transition kernel that generates new proposals
+        :return: Updated Samples
+        """
+        self.proposal_distrib = proposal_distrib if proposal_distrib else self.proposal_distrib
+        self.trans_kern = trans_kern if trans_kern else self.trans_kern
+
+        for t in range(T):
+            for idx, _x in enumerate(range(self.x)):
+                for idx2, _xi in enumerate(_x):
+                    _xi_0, _x_temp = _xi, _x
+                    proposed_xi = _xi_0 + self.trans_kern(loc=self.loc, scale=self.scale)
+                    _x_temp[idx2] = proposed_xi
+
+                    num = self.pi(_x_temp)*self.proposal_distrib(_xi_0, proposed_xi)
+                    den = self.pi(_x)*self.proposal_distrib(proposed_xi, _xi_0)
+                    A = min(1, num/den)
+                    if uniform.rvs() < A:
+                        _x[idx2] = proposed_xi
+                self.x[idx] = _x
+
+        return self.x
+
+
+
 
 
